d6077441996c4aff0507f40e0490f747b5eb3059
jiwon Moon <mon99745@dreamsecurity.com>
2024-03-11 17:47:46 +0900

    Add. SpringDoc 관리 - Swagger v3 추가

--- src/main/java/com/example/sample/controller/TestController.java
+++ src/main/java/com/example/sample/controller/TestController.java
@@ -2,11 +2,13 @@ package com.example.sample.controller;
 
 import com.example.common.exception.CommonError;
 import com.example.common.exception.CommonException;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
+	@Operation(summary = "Exception Test")
 	@GetMapping("exception")
 	public void exception() {
 		throw new CommonException(CommonError.COM_TEST);
